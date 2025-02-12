227a4ecd970e8e220439993cb0b376d6d0959dd5
JamaluddinAkbar <151990444+JamaluddinAkbar@users.noreply.github.com>
2025-02-12 08:08:19 +0700

    Update dice_roller.dart

--- dice_roller.dart
+++ dice_roller.dart
@@ -44,7 +44,7 @@ class _DiceRollerState extends State<dice_roller1> {
             padding: const EdgeInsets.only(
               top: 20,
             ),
-            foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+            foregroundColor: const Color.fromARGB(255, 0, 0, 0),
             textStyle: TextStyle(fontSize: 28),
           ),
           child: const Text('Roll Dice!'),
