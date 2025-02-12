acc118ffccbbfee60ee15a46391dc9f63b709d89
JamaluddinAkbar <151990444+JamaluddinAkbar@users.noreply.github.com>
2025-02-12 07:54:56 +0700

    Update dice_roller3.dart

--- dice_roller3.dart
+++ dice_roller3.dart
@@ -5,6 +5,7 @@ import 'dart:math';
 
 final randomizer1 = Random();
 final randomizer2 = Random();
+final randomizer3 = Random();
 
 class dice_roller3 extends StatefulWidget {
   const dice_roller3({super.key});
@@ -17,12 +18,14 @@ class dice_roller3 extends StatefulWidget {
 
 class _DiceRollerState extends State<dice_roller3> {
   var currentDiceRoll1 = 2;
-  var currentDiceRoll2 = 2;
+  var currentDiceRoll2 = 3;
+  var currentDiceRoll3 = 4;
 
   void rollDice() {
     setState(() {
       currentDiceRoll1 = randomizer1.nextInt(6) + 1;
       currentDiceRoll2 = randomizer2.nextInt(6) + 1;
+      currentDiceRoll3 = randomizer3.nextInt(6) + 1;
     });
   }
 
@@ -37,6 +40,8 @@ class _DiceRollerState extends State<dice_roller3> {
             Image.asset('assets/dice-$currentDiceRoll1.png', width: 150),
             SizedBox(width: 20),
             Image.asset('assets/dice-$currentDiceRoll2.png', width: 150),
+            SizedBox(width: 20),
+            Image.asset('assets/dice-$currentDiceRoll3.png', width: 150),
           ],
         ),
         TextButton(
@@ -45,7 +50,7 @@ class _DiceRollerState extends State<dice_roller3> {
             padding: const EdgeInsets.only(
               top: 20,
             ),
-            foregroundColor: Colors.white,
+            foregroundColor: const Color.fromARGB(255, 255, 255, 255),
             textStyle: TextStyle(fontSize: 28),
           ),
           child: const Text('Roll Dice!'),
